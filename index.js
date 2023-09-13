b5cf5b5631f5bc4b99acc8ce8410caeda75ba2b9
Javier Labbé <javier.labbe@uc.cl>
2023-09-12 22:23:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,26 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function(req, res) {
+  const ip = req.headers['x-forwarded-for']
+  const idioma = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+ 
+  let resObj = {
+    ipaddress: ip,
+    language: idioma,
+    software: software
+  }
+  
+  res.json(resObj);
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
